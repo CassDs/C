72be4982c97a15f70b18f35981680c8fe135ffc9
Cássio <cassioms764@gmail.com>
2020-05-28 21:54:30 -0300

    primeiro commit

---
+++ main_excript.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <locale.h>
+#include <conio.h>
+
+int main(){
+	system("color 9");
+	setlocale(LC_ALL, "");
+	int version = 18;
+	char exit;
+	
+    printf("________________________________________\n");                          
+	printf("|                                      |\n");                                            
+	printf("|               CCCCCCCCCCCCC          |\n");
+	printf("|            CCC::::::::::::C          |\n");
+	printf("|          CC:::::::::::::::C          |\n");
+	printf("|         C:::::CCCCCCCC::::C          |\n");
+	printf("|        C:::::C       CCCCCC          |\n");
+	printf("|       C:::::C                        |\n");
+	printf("|       C:::::C                        |\n");
+	printf("|       C:::::C                        |\n");
+	printf("|       C:::::C                        |\n");
+	printf("|       C:::::C                        |\n");
+	printf("|       C:::::C                        |\n");
+	printf("|       C:::::C        CCCCCC          |\n");
+	printf("|         C:::::CCCCCCCC::::C          |\n");
+	printf("|          CC:::::::::::::::C          |\n");
+	printf("|            CCC::::::::::::C          |\n");
+	printf("|               CCCCCCCCCCCCC          |\n");
+	printf("|______________________________________|\n");
+	printf("\n");
+	printf("Criador: Dennis Ritchie\n");
+	printf("Ano de criação: 1972\n");
+	printf("Versão: C%d \n", version);
+	scanf("%c",&exit);
+	system("cls");
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+
+	return 0;
+}
+"# C" 
